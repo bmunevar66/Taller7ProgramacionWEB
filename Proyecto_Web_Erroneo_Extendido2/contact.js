f456a67f82e79a11b1cb8c4e5ba88c6aeda4413d
bmunevar66 <bmunevar@ucundinamarca.edu.co>
2025-05-07 22:08:34 -0500

    Correciones en el .js
    Se corrigen erroes y se agregar funciones para hacer mas funcional el codigo

--- Proyecto_Web_Erroneo_Extendido2/contact.js
+++ Proyecto_Web_Erroneo_Extendido2/contact.js
@@ -1,5 +1,16 @@
 function enviar() {
     let email = document.getElementById('email').value;
-    alert("Gracias por contactarnos " + email);
-    document.getElementById('msg').innerHTML = "<script>alert('XSS en mensaje')</script>";
+    document.getElementById('msg').textContent("Gracias por contactarnos, " + email);// se cambia el Alert ya que es ambiguo
+    //viendo la linea 6 en terminos de seguridad, esta puede permitir la ejecucion de codigos maliciosos
+    //hay que evitar las inserciones directas, entonces se cambia innerHTML por textContect, y se arregla el mensaje
+    document.getElementById('msg').textContent = 'mensaje recibido con exito';
 }
+//y se agrega una nueva funcion para la validacion de los datos del formulario
+function validarFormulario(){
+    let nombre = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    if(!nombre || !email){
+        alert('Los campos no pueden estar vacios');
+        return false;
+    }
+}return true;

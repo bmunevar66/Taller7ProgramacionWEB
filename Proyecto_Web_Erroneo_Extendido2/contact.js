bba646e46a05a0eeb8b34638b92047e409b5f2ba
bmunevar66 <bmunevar@ucundinamarca.edu.co>
2025-05-07 22:48:41 -0500

    Mejoras y cambios
    Se haces mejoras en el codigo respecto a seguridad, accesibilidad y vista de usuario

--- Proyecto_Web_Erroneo_Extendido2/contact.js
+++ Proyecto_Web_Erroneo_Extendido2/contact.js
@@ -1,6 +1,8 @@
 function enviar() {
-    let email = document.getElementById('email').value;
-    document.getElementById('msg').textContent("Gracias por contactarnos, " + email);// se cambia el Alert ya que es ambiguo
+    const email = document.getElementById('email').value.trim();//Se agregan validaciones para evitar espacion inecesarios en los campos
+    const nombre = document.getElementById('nombre').value.trim();
+    const mensaje = document.getElementById('msg').value.trim;
+    const rsta = document.getElementById('rsta');
     //viendo la linea 6 en terminos de seguridad, esta puede permitir la ejecucion de codigos maliciosos
     //hay que evitar las inserciones directas, entonces se cambia innerHTML por textContect, y se arregla el mensaje
     document.getElementById('msg').textContent = 'mensaje recibido con exito';
